8db0c657417ff45caf23bf290c0af587a1720ba5
zhang <30815872@qq.com>
2018-03-26 16:45:27 +0800

    正则匹配期号

--- gethistroy.py
+++ gethistroy.py
@@ -18,6 +18,27 @@ class Gethistroydate:
     data = data.decode('utf-8')
     # 打印结果
     print(data)
+    print("————————————————————————————")
+    # abc1 = re.findall(r'<td class="bg_5">(.*)</td>',data,re.M|re.I)
+    # df1 = {'期号':None,'红球':None,'蓝球':None}
+    # a1 = []
+    # for matchObj in abc1:
+    #     a1.append(matchObj)
+    #     df1 = {'期号':a1}
+    #     print(matchObj)
+    # print(a1)
+    # print(df1)
+    abc2 = re.findall(r'ball_red\'>()</td>',data,re.I|re.M)
+    print(abc2)
+    df1 = {'期号':None,'红球':None,'蓝球':None}
+    a2 = []
+    # for matchObj in abc2:
+    #     a2.append(matchObj)
+    #     df1 = {'红球':a2}
+    #     print(matchObj)
+    # print(a2)
+    # print(df1)
 
-    matchObj = re.search(r'<td class="bg_5"></td>',data,re.M|re.I)
-    print(matchObj)
+
+
+
